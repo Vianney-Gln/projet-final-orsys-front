ead7705558ebb1862cf471ca144d820d3d12e0fe
Vianney-Gln <vgeloen@gmail.com>
2023-01-27 17:54:35 +0100

    feat:deconnexion ok

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+  styleUrls: ['./navigation.component.css'],
 })
 export class NavigationComponent {
+  constructor(private router: Router) {}
 
+  deconnexion() {
+    localStorage.removeItem('utilisateurId');
+    this.router.navigateByUrl('/');
+  }
 }
