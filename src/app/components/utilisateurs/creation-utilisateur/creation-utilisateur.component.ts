e941f5df3246083a98511dd729e8686e5d257a58
Vianney-Gln <vgeloen@gmail.com>
2023-01-31 18:35:59 +0100

    creation des services de recuperation des liens de parentes et des pays

--- src/app/components/utilisateurs/creation-utilisateur/creation-utilisateur.component.ts
+++ src/app/components/utilisateurs/creation-utilisateur/creation-utilisateur.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { LienDeParenteService } from 'src/app/services/lien-de-parente.service';
+import { PaysService } from 'src/app/services/pays.service';
+import { Pays } from 'src/models/Pays';
+import { LienDeParente } from '../../../../models/LiendeParente';
 
 @Component({
   selector: 'app-creation-utilisateur',
@@ -7,7 +11,55 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./creation-utilisateur.component.css'],
 })
 export class CreationUtilisateurComponent {
+  constructor(
+    private paysService: PaysService,
+    private lienDeParenteService: LienDeParenteService
+  ) {}
+
+  confirmedPassword: string = '';
+  pays: Pays[] = [];
+  lienDeParente: LienDeParente[] = [];
+
+  /**
+   * Fonction qui traite l'envois de l'inscription
+   * @param f
+   */
   handleSubmit(f: NgForm) {
-    console.log(f.valid);
+    console.log(f.value);
+  }
+
+  /**
+   * Fonction qui récupère la confirmation du password
+   * @param e
+   */
+  getConfirmPassword(e: any) {
+    this.confirmedPassword = e.value;
+    console.log(this.confirmedPassword);
+  }
+
+  getPays(): void {
+    this.paysService.getPays().subscribe({
+      next: (resp) => {
+        this.pays = resp;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+  getLienDeParentes(): void {
+    this.lienDeParenteService.getLienDeParentes().subscribe({
+      next: (resp) => {
+        this.lienDeParente = resp;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  ngOnInit() {
+    this.getPays();
+    this.getLienDeParentes();
   }
 }
