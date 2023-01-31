3c11e3627fce2131f3cd6475c9beddc3cf6ec123
Vianney-Gln <vgeloen@gmail.com>
2023-01-31 17:56:07 +0100

    etablissement d'une base formulaire

--- src/app/components/utilisateurs/creation-utilisateur/creation-utilisateur.component.ts
+++ src/app/components/utilisateurs/creation-utilisateur/creation-utilisateur.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-creation-utilisateur',
   templateUrl: './creation-utilisateur.component.html',
-  styleUrls: ['./creation-utilisateur.component.css']
+  styleUrls: ['./creation-utilisateur.component.css'],
 })
 export class CreationUtilisateurComponent {
-
+  handleSubmit(f: NgForm) {
+    console.log(f.valid);
+  }
 }
